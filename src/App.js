5d59ac20c2f9add132e3709888ebe09d3944e0cb
krishna kumar <krishnau2@gmail.com>
2019-01-09 23:06:33 +0530

    initial commit with full layout

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container text-center">
+        <div className="card">
+          <span className="score">05 / 01</span><br />
+          <span className="overs">Overs: 0.5</span>
+        </div>
+        <div className="points-wrap">
+          <button className="point">1</button>
+          <button className="point">2</button>
+          <button className="point">3</button>
+          <button className="point">4</button>
+          <button className="point">6</button>
+        </div>
+        <div className="match-actions-wrap">
+          <button className="match-actions " disabled="disabled">Wicket !</button>
+          <button className="match-actions " disabled="disabled">No Ball</button>
+          <button className="match-actions" disabled="disabled">Wide</button>
+        </div>
       </div>
     );
   }
